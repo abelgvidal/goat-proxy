161fcba76bcdb435804d4f22ed9a92e77e6b52f8
Abel Gonzalez <abel@nextail.co>
2026-03-14 23:44:11 +0100

    First version: just connecting ends

--- main.go
+++ main.go
@@ -1,15 +1,49 @@
 package main
 
 import (
-	"fmt"
+	"bufio"
+	"io"
+	"log"
+	"net"
 	"net/http"
 )
 
 func main() {
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "I hear you in :9009\n")
+		// conectar con el backend
+		backendConn, err := net.Dial("tcp", "localhost:8500")
+		if err != nil {
+			log.Fatalf("Error connecting to backend: %v", err)
+		}
+		defer backendConn.Close()
+
+		// manejar las solicitudes HTTP
+		r.Write(backendConn)
+
+		// 3. Leer la respuesta del backend y copiarla al cliente
+		backendResp, err := http.ReadResponse(
+			bufio.NewReader(backendConn),
+			r,
+		)
+		if err != nil {
+			http.Error(w, "bad response", http.StatusBadGateway)
+			return
+		}
+		defer backendResp.Body.Close()
+
+		// 4. Copiar cabeceras y status
+		for key, values := range backendResp.Header {
+			for _, v := range values {
+				w.Header().Add(key, v)
+			}
+		}
+		w.WriteHeader(backendResp.StatusCode)
+
+		// 5. Copiar el body
+		io.Copy(w, backendResp.Body)
 	})
 
-	fmt.Println("I hear you in :9009")
-	http.ListenAndServe(":9009", nil)
+	log.Println("FW-Proxy listening on :8080")
+	http.ListenAndServe(":8080", nil)
 }
